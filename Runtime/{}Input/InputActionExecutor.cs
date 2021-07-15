dc3ee0fc4a38d4d3164338422012f87228ff2f81
Pixel Lifetime <maxymkarpinsky@gmail.com>
2021-07-15 18:55:27 +0300

    GJL game jam changes.

--- Runtime/{}Input/InputActionExecutor.cs
+++ Runtime/{}Input/InputActionExecutor.cs
@@ -1,25 +1,114 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
-public class InputActionExecutor : InputActionExecutor<InputAction>
-{
-	protected override void InitializeActions()
-	{
-		this.inputAction.started += this.onActionStarted.Invoke;
-		this.inputAction.performed += this.onActionPerformed.Invoke;
-		this.inputAction.canceled += this.onActionCanceled.Invoke;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-		this.inputAction.Enable();
+namespace PixLi
+{
+	public abstract class InputActionExecutor<TInputAction> : MonoBehaviour
+	{
+		[SerializeField] protected UnityEvent<InputAction.CallbackContext> onActionStarted;
+		public UnityEvent<InputAction.CallbackContext> _OnActionStarted { get { return this.onActionStarted; } }
+		
+		[SerializeField] protected UnityEvent<InputAction.CallbackContext> onActionPerformed;
+		public UnityEvent<InputAction.CallbackContext> _OnActionPerformed { get { return this.onActionPerformed; } }
+		
+		[SerializeField] protected UnityEvent<InputAction.CallbackContext> onActionCanceled;
+		public UnityEvent<InputAction.CallbackContext> _OnActionCanceled { get { return this.onActionCanceled; } }
+
+		[SerializeField] protected TInputAction inputAction;
+		public TInputAction _InputAction => this.inputAction;
+
+		public bool Active_ { get; protected set; }
+
+		protected abstract void InitializeActions();
+		public virtual void Activate()
+		{
+			if (this.Active_)
+				return;
+
+			this.InitializeActions();
+
+			this.Active_ = true;
+		}
+
+		protected abstract void DeInitializeActions();
+		public virtual void Deactivate()
+		{
+			if (!this.Active_)
+				return;
+
+			this.DeInitializeActions();
+
+			this.Active_ = false;
+		}
+
+#if UNITY_EDITOR
+		//protected override void OnDrawGizmos()
+		//{
+		//}
+#endif
 	}
 
-	protected override void DeInitializeActions()
+	public class InputActionExecutor : InputActionExecutor<InputAction>
 	{
-		this.inputAction.started -= this.onActionStarted.Invoke;
-		this.inputAction.performed -= this.onActionPerformed.Invoke;
-		this.inputAction.canceled -= this.onActionCanceled.Invoke;
+		protected override void InitializeActions()
+		{
+			this.inputAction.started += this.onActionStarted.Invoke;
+			this.inputAction.performed += this.onActionPerformed.Invoke;
+			this.inputAction.canceled += this.onActionCanceled.Invoke;
 
-		this.inputAction.Disable();
+			this.inputAction.Enable();
+		}
+
+		protected override void DeInitializeActions()
+		{
+			this.inputAction.started -= this.onActionStarted.Invoke;
+			this.inputAction.performed -= this.onActionPerformed.Invoke;
+			this.inputAction.canceled -= this.onActionCanceled.Invoke;
+
+			this.inputAction.Disable();
+		}
+
+#if UNITY_EDITOR
+		[CustomEditor(typeof(InputActionExecutor))]
+		[CanEditMultipleObjects]
+		public class InputActionExecutorEditor : Editor
+		{
+#pragma warning disable 0219, 414
+			private InputActionExecutor _sInputActionExecutor;
+#pragma warning restore 0219, 414
+
+			private void OnEnable()
+			{
+				this._sInputActionExecutor = this.target as InputActionExecutor;
+			}
+
+			private bool _eventsFoldout;
+
+			public override void OnInspectorGUI()
+			{
+				this._eventsFoldout = EditorGUILayout.Foldout(this._eventsFoldout, new GUIContent("Events"), true);
+
+				if (this._eventsFoldout)
+				{
+					EditorGUILayout.PropertyField(this.serializedObject.FindProperty("onActionStarted"), true);
+					EditorGUILayout.PropertyField(this.serializedObject.FindProperty("onActionPerformed"), true);
+					EditorGUILayout.PropertyField(this.serializedObject.FindProperty("onActionCanceled"), true);
+				}
+
+				EditorGUILayout.PropertyField(this.serializedObject.FindProperty("inputAction"), true);
+
+				this.serializedObject.ApplyModifiedProperties();
+			}
+		}
+#endif
 	}
 }
--- Runtime/{}Input/InputActionReferenceExecutor.cs
+++ Runtime/{}Input/InputActionReferenceExecutor.cs
@@ -1,74 +1,60 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
-[Serializable]
-public class UnityEventCallbackContext : UnityEvent<InputAction.CallbackContext> { }
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-public abstract class InputActionExecutor<TInputAction> : MonoBehaviour
+namespace PixLi
 {
-	[SerializeField] protected UnityEventCallbackContext onActionStarted;
-	public UnityEventCallbackContext _OnActionStarted { get { return this.onActionStarted; } }
-	
-	[SerializeField] protected UnityEventCallbackContext onActionPerformed;
-	public UnityEventCallbackContext _OnActionPerformed { get { return this.onActionPerformed; } }
-	
-	[SerializeField] protected UnityEventCallbackContext onActionCanceled;
-	public UnityEventCallbackContext _OnActionCanceled { get { return this.onActionCanceled; } }
-
-	[SerializeField] protected TInputAction inputAction;
-
-	public bool Active_ { get; protected set; }
-
-	protected abstract void InitializeActions();
-	public virtual void Activate()
+	public class InputActionReferenceExecutor : InputActionExecutor<InputActionReference>
 	{
-		if (this.Active_)
-			return;
+		protected override void InitializeActions()
+		{
+			this.inputAction.action.started += this.onActionStarted.Invoke;
+			this.inputAction.action.performed += this.onActionPerformed.Invoke;
+			this.inputAction.action.canceled += this.onActionCanceled.Invoke;
 
-		this.InitializeActions();
+			this.inputAction.action.Enable();
+		}
 
-		this.Active_ = true;
-	}
+		protected override void DeInitializeActions()
+		{
+			this.inputAction.action.started -= this.onActionStarted.Invoke;
+			this.inputAction.action.performed -= this.onActionPerformed.Invoke;
+			this.inputAction.action.canceled -= this.onActionCanceled.Invoke;
 
-	protected abstract void DeInitializeActions();
-	public virtual void Deactivate()
-	{
-		if (!this.Active_)
-			return;
-
-		this.DeInitializeActions();
-
-		this.Active_ = false;
-	}
+			this.inputAction.action.Disable();
+		}
 
 #if UNITY_EDITOR
+		//protected override void OnDrawGizmos()
+		//{
+		//}
+		
+		[CustomEditor(typeof(InputActionReferenceExecutor))]
+		[CanEditMultipleObjects]
+		public class InputActionReferenceExecutorEditor : Editor
+		{
+#pragma warning disable 0219, 414
+			private InputActionReferenceExecutor _sInputActionReferenceExecutor;
+#pragma warning restore 0219, 414
+
+			private void OnEnable()
+			{
+				this._sInputActionReferenceExecutor = this.target as InputActionReferenceExecutor;
+			}
+
+			public override void OnInspectorGUI()
+			{
+				this.DrawDefaultInspector();
+			}
+		}
 #endif
-}
-
-public class InputActionReferenceExecutor : InputActionExecutor<InputActionReference>
-{
-	protected override void InitializeActions()
-	{
-		this.inputAction.action.started += this.onActionStarted.Invoke;
-		this.inputAction.action.performed += this.onActionPerformed.Invoke;
-		this.inputAction.action.canceled += this.onActionCanceled.Invoke;
-
-		this.inputAction.action.Enable();
 	}
-
-	protected override void DeInitializeActions()
-	{
-		this.inputAction.action.started -= this.onActionStarted.Invoke;
-		this.inputAction.action.performed -= this.onActionPerformed.Invoke;
-		this.inputAction.action.canceled -= this.onActionCanceled.Invoke;
-
-		this.inputAction.action.Disable();
-	}
-
-#if UNITY_EDITOR
-#endif
 }
